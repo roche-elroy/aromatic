6d4d31118703a4cbf174232626874fe972002688
roche-elroy <23h22.jeethan@sjec.ac.in>
2025-03-10 19:55:05 +0530

    feat/location: created location page

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import CameraScreen from './src/components/camera/Camera';
 // import SettingsScreen from '.src/components/settings/Settings';
-// import EmergencyScreen from '.src/components/emergency/Emergency';
+import EmergencyScreen from './src/components/emergency/Emergency';
 // import LocationScreen from './src/components/location/Location';
 // import ProfileScreen from './src/components/profile/Profile';
 
