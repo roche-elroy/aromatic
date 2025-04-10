b00f32088c35a35d433c0d2bea72199fe3910f58
roche-jeethan <23h22.jeethan@sjec.ac.in>
2025-04-10 18:51:00 +0530

    added expo-system-ui

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 
-// export const SERVER_IP = '192.168.43.22';
+export const SERVER_IP = '192.168.43.22';
 
-export const SERVER_IP =   '172.25.2.87';
+// export const SERVER_IP =   '172.25.2.87';
 
 // export const ENDPOINTS = {
 //     development: '192.168.1.1',
